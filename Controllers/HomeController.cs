ab782a28f9519aa75f1bf13e098c4857a5056c61
Craig J Hannon <c.hannon@nditech.com>
2019-05-14 15:55:42 +0100

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace magicmirror.Controllers{
     public class HomeController: Controller
     {
+        private IHttpClientFactory _httpClientFactory;
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory=httpClientFactory;
+        }
+
         public IActionResult Index () => View();
+
+        public IActionResult GetWeather() 
+        {
+
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace magicmirror
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddMvc();
         }
 
